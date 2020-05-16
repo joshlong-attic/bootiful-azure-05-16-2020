fd1e7bd47b849121d6bc24a04887bf233f3c1ca5
Josh Long <josh@joshlong.com>
2020-05-16 04:15:45 -0700

    polush

--- bootiful-azure/src/main/java/com/example/bootifulazure/BootifulAzureApplication.java
+++ bootiful-azure/src/main/java/com/example/bootifulazure/BootifulAzureApplication.java
@@ -1,24 +1,19 @@
 package com.example.bootifulazure;
 
-import com.microsoft.azure.servicebus.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -28,11 +23,6 @@ public class BootifulAzureApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootifulAzureApplication.class, args);
-    }
-
-    @Bean
-    ExecutorService executorService() {
-        return Executors.newCachedThreadPool();
     }
 }
 
@@ -45,8 +35,8 @@ class SpringCloudStreamServiceBusDemo {
     private final Source source;
 
     @GetMapping("/send")
-    void send() {
-        this.source.output().send(MessageBuilder.withPayload("Hello bootiful service bus @ " + Instant.now() + "!").build());
+    public void send() {
+        this.source.output().send(MessageBuilder.withPayload("Hello bootiful Azure Service Bus @ " + Instant.now() + "!").build());
     }
 
     @StreamListener(Sink.INPUT)
